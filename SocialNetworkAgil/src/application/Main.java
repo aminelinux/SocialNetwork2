e455f66724fef97adc8aa8aade1e3e3289279d2a
aminelinux <aminehamdi1992@gmail.com>
2022-02-18 15:20:40 +0100

    final log in inteface

--- SocialNetworkAgil/src/application/Main.java
+++ SocialNetworkAgil/src/application/Main.java
@@ -1,8 +1,9 @@
 package application;
-
+	
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -20,19 +21,17 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
 	private StackPane root = new StackPane();
     private Stage stage;
+
     @Override
     public void init() {
     	VBox vBox = new VBox();
     	VBox vBox1 = new VBox();
-    	HBox hBoxCenter = new HBox();
-    	HBox hboxTop = new HBox();
-    	HBox hboxSignIn = new HBox();
+    	HBox hBox = new HBox();
     	BorderPane pane = new BorderPane();
     	Label labelApp = new Label();
         Button buttonLogin = new Button("Connect");
@@ -44,6 +43,13 @@ public class Main extends Application {
         Label passLabel = new Label("Password");
         TextField userText = new TextField();
         PasswordField passText = new PasswordField();
+        HBox hboxTop = new HBox();
+        HBox boxDown = new HBox();
+        
+        //-fx-border-color: #ff0000;-fx-border-width: 1px;
+        buttonExit.setStyle("-fx-background-color: #e64e4e;-fx-text-fill: #fff0f0;");
+        buttonSignIn.setStyle("-fx-background-color: #e64e4e;-fx-text-fill: #fff0f0;");
+        
         InputStream stream = null;
         final ImageView selectedImage = new ImageView();
         try {
@@ -59,37 +65,58 @@ public class Main extends Application {
         hboxTop.setAlignment(Pos.CENTER);
         HBox.setMargin(pane, new Insets(10,10,10,10));     
         hboxTop.getChildren().addAll(selectedImage);
-        buttonExit.setStyle("-fx-background-color: #e64e4e;-fx-text-fill: #fff0f0;");
-        buttonSignIn.setStyle("-fx-background-color: #e64e4e;-fx-text-fill: #fff0f0;");
-        hBoxCenter.setSpacing(10);
-        hBoxCenter.setPadding(new Insets(10,10,10,10));
+       // userNameLabel.setUnderline(true);
+        boxDown.setAlignment(Pos.CENTER);
+        Label lRegister = new Label("Still not registred in ");
+        Button bRegister = new Button("Register");
+        boxDown.getChildren().addAll(lRegister,bRegister);
+        labelApp.setText("Sign In To app");
+        //labelApp.setStyle();
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(10,10,10,10));
         
-        hboxSignIn.getChildren().addAll(buttonSignIn,buttonExit);
-        hboxSignIn.setSpacing(285);
         vBox.setSpacing(8);
         vBox.setPadding(new Insets(10,10,10,10));
+        
+       
         vBox.setAlignment(Pos.CENTER);
         vBox.getChildren().addAll(
+        		hboxTop,
+        		hBox,
         		userNameLabel,
         		userText,
                 passLabel,
                 passText,
                 buttonLogin,
-                hboxSignIn);
-        pane.setTop(hboxTop);
-        pane.setCenter(vBox);
-        root.getChildren().addAll(pane);
+                boxDown,
+                buttonExit);
         
-        
+        //root.getChildren().addAll(vBox);
+        //pane.setTop(labelApp);
+        //pane.getChildren().addAll(vBox);
+        root.getChildren().addAll(vBox);
         buttonExit.setOnAction(new EventHandler<ActionEvent>() {
+
 			@Override
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
 				System.exit(0);
-			}     	
+			}
+        	
         });
+
+        /*button.setOnAction(actionEvent-> {
+            if(stage!=null){
+                stage.requestFocus();
+                return;
+            }
+            stage = new Stage();
+            StackPane stackPane = new StackPane();
+            stage.setScene(new Scene(stackPane, 200,200));
+            stage.show();
+        }); */
     } 
-    
+
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(root,400,600);
@@ -97,8 +124,9 @@ public class Main extends Application {
         primaryStage.show();
         primaryStage.setTitle("Login");
         primaryStage.setAlwaysOnTop(true);
-        primaryStage.setResizable(false);
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
